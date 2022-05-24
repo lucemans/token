158846cb251a93b306706991fbcbdeb6d393126e
Lucemans <luc@lucemans.nl>
2022-05-24 19:33:50 +0200

    Update Image

--- dot/src/Contract.sol
+++ dot/src/Contract.sol
@@ -40,11 +40,35 @@ contract Contract {
 
     function tokenURI(uint256 _tokenId) public view returns (string memory) {
         require(_ownerOf[_tokenId] != address(0));
-        return string(abi.encodePacked(uri, _tokenId, ".json"));
+        return string(abi.encodePacked(uri, toString(_tokenId), ".json"));
     }
 
     function contractURI() public view returns (string memory) {
         return string(abi.encodePacked(uri, "root.json"));
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
+     * Inspired by OpenZeppelin's implementation - MIT license
+     * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol
+     */
+    function toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 
     /*/////////////////////////////////////////////////////////////
